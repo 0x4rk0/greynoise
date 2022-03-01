3ad225cba1470b774888a68f79bbe9ed31aaafdd
0x4rk0 <cody@bernardy.biz>
2022-03-01 17:16:37 +0000

    Updating README

--- CISA/README.md
+++ CISA/README.md
@@ -1,2 +1,5 @@
 # This contains useful GNQL queries and scripts on Greynoise that pertain to the CISA Known Exploited Vulnerabilities Catalog.
 
+##Usage
+'''bash
+sudo chmod +x updater.sh && ./updater.sh
---
+++ README.d
@@ -0,0 +1,8 @@
+# Greynoise Scripts and Findings
+This repo contains some of my favorite GNQL searches as well as different ways I am able to prioritize Vulnerabilities on Greynoise
+
+## Contributing
+Please make any pull requests or fork this repo to add onto what currently exists.
+
+##License
+[MIT](https://choosealicense.com/licenses/mit/)
