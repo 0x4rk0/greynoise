3c9cc2b983e062ea626a2345906e00ad357b7566
0x4rk0 <cody@bernardy.biz>
2022-03-02 18:30:42 +0000

    Removing Append from greynoise_cisa.sh

--- CISA/greynoise_cisa.sh
+++ CISA/greynoise_cisa.sh
@@ -2,5 +2,5 @@
 
 for i in $(cat cves.txt)
 do
-  curl -L -X GET 'https://api.greynoise.io/v2/experimental/gnql/stats?query=cve:'$i'' -H 'key: YOU THOUGHT' | jq | tee -a output.json
+  curl -L -X GET 'https://api.greynoise.io/v2/experimental/gnql/stats?query=cve:'$i'' -H 'key: YOU THOUGHT' | jq | tee  output.json
 done
