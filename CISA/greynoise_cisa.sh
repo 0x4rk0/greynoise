f7469c0e750f38d863629dabbbf536d46653a861
0x4rk0 <cody@bernardy.biz>
2022-03-02 18:34:04 +0000

    Changing file for greynoise_cisa.sh, I should really do testing before pushing hella commits lmao

--- CISA/greynoise_cisa.sh
+++ CISA/greynoise_cisa.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(cat cves.txt)
+for i in $(cat known_exploited_vulnerabilities.csv)
 do
   curl -L -X GET 'https://api.greynoise.io/v2/experimental/gnql/stats?query=cve:'$i'' -H 'key: YOU THOUGHT' | jq | tee  output.json
 done
